b403bc08763ba363d42f3f930fd5baaef741055c
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-05-04 22:01:08 +0700

    update: adding images, set font, some adjustment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import {FcGoogle} from "react-icons/fc";
 import {FaFacebook} from "react-icons/fa";
+
+//IMAGES
+import peopleBg from "../assets/images/picture.png"
+
 const Login = () => {
   return(
   
     <div className="flex h-screen">
-      <div className="flex-1 bg-accent"></div>
+      <div className="flex-1 bg-accent">
+        <div className="flex justify-end items-center h-screen">
+          <img className="w-[500px]" src={peopleBg} alt="picture" />
+        </div>
+      </div>
       <div className="flex flex-col max-w-md w-full justify-center items-center gap-8">
-        <div className="w-[80%] flex flex-col gap-5">
-          <div className="text-5xl font-bold">Sign In</div>
-          <div className="">Hi Welcome back to Ur Ticket</div>
+        <div className="w-[80%] flex flex-col gap-8">
+          <div className="text-3xl font-bold">Sign In</div>
+          <div className="">Hi, Welcome back to Urticket! </div>
         </div>
         <form className="w-[80%] flex flex-col gap-5">
           <div>
@@ -22,11 +30,11 @@ const Login = () => {
             <input className="input input-bordered border-1 w-full" type="password" name="password" placeholder="Password" />
           </div>
           <div className="text-right">
-            <Link className="hover:text-secondary"to="/forgot-password">Forgot Password</Link>
+            <Link className="hover:text-secondary bold text-[#38291B]"to="/forgot-password">Forgot Password</Link>
           </div>
           <div className="flex flex-col justify-center items-center gap-5">
             <div className="w-full text-center">
-              <button className="btn btn-accent hover:btn-secondary btn-block">Sign In</button>
+              <button className="btn btn-accent hover:btn-secondary btn-block normal-case">Sign In</button>
             </div>
             <div>or sign in with</div>
           </div>
@@ -40,6 +48,8 @@ const Login = () => {
           </div>
         </form>
       </div>
+      
+      
     </div>
   
   )
