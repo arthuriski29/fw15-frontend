5a5a4141de16f3148b866c3708f9bbea8fee3d19
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-05-11 00:45:21 +0700

    get data per page event detail

--- src/pages/DetailEvent.jsx
+++ src/pages/DetailEvent.jsx
@@ -1,6 +1,25 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import http from "../helpers/http";
+
 const Details = () => {
+  const {id} = useParams()
+  const [event, setEvent] = React.useState({})
+  React.useEffect(() => {
+    const getEventData = async() => {
+      const {data} = await http().get(`/events/${id}`, )
+      setEvent(data.results)
+    }
+    if(id){
+      getEventData(id)
+    }
+  }, [id])
+  
   return(
-    <>Detail Event</>
+    <div className="flex justify-center flex-col h-screen items-center text-5xl">
+      <div>{event?.event}</div>
+      <div>{event?.location}</div>
+    </div>
   )
 }
 export default Details;
