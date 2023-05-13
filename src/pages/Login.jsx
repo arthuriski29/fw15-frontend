cf4e99f188feef4697f2d554e1a69873db8b4331
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-05-13 21:02:28 +0700

    update: formik, prop, validationSchema, dispatch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,148 @@
 import { Link } from "react-router-dom";
 import {FcGoogle} from "react-icons/fc";
 import {FaFacebook} from "react-icons/fa";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import http from "../helpers/http";
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import propTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+
+import { clearMessage, login, setErrorMessage } from "../redux/reducers/auth"
 
 //IMAGES
 import peopleBg from "../assets/images/picture.png"
 import weTick from "../assets/images/wetick-logo.png"
 
-const Login = () => {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const [errorMessage, setErrorMessage] = React.useState('')
-  const [warningMessage, setWarningMessage] = React.useState(location.state?.warningMessage)
-  const [token, setToken] = React.useState('')
-  const doLogin = async (event) => {
-    event.preventDefault()
-    setErrorMessage('')
-    setWarningMessage('')
-    try {
-      const {value: email} = event.target.email
-      const {value: password} = event.target.password
-      const body = new URLSearchParams({email, password}).toString() //mengambil query string
-      const {data} = await http().post('http://localhost:8888/auth/login', body)
-      window.localStorage.setItem('token', data.results.token)
-      setToken(data.results.token)
-    }catch (err) {
-      const message = err?.response?.data?.message
-      if(message){
-        setErrorMessage(message)
-      } 
-    }
+const validationSchema = Yup.object({
+  email: Yup.string().email('Email is invalid'),
+  password: Yup.string().required('Password is invalid')
+})
 
-  }
-  React.useEffect(()=>{
-    
-    if (token){
-      console.log('test')
-      navigate('/')
+const FormLogin = ({values, errors, touched, handleBlur, handleChange, handleSubmit, isSubmitting}) => {
+  const errorMessage = useSelector(state => state.auth.errorMessage)
+  const warningMessage = useSelector(state => state.auth.warningMessage)
+  return (
+    <form onSubmit={handleSubmit} className="w-[80%] flex flex-col gap-5">
+      <div className="w-[80%] flex flex-col gap-5">
+        <div className="text-3xl font-bold">Sign In</div>
+        <div className="">Hi, Welcome back to Urticket! </div>
+        {warningMessage && 
+        (<div>
+          <div className="alert alert-warning">{warningMessage}</div>
+        </div> )}
+        {errorMessage && 
+        (<div>
+          <div className="alert alert-error">{errorMessage}</div>
+        </div> )}
+      </div>
+      <div className="form-control">
+        <input 
+          className={`input input-bordered border-1 w-full ${errors.email && touched.email && 'input-error'}`} 
+          type="email" 
+          name="email" 
+          placeholder="Email" 
+          onChange={handleChange} 
+          onBlur={handleBlur}
+          value={values.email} 
+        />
+        {errors.email && touched.email && (
+            <label className="label">
+              <span className="label-text-alt text-error"></span>
+              <span className="label-text-alt text-error">{errors.email}</span>
+            </label>
+        )}
+
+      </div>
+
+      <div className="form-control">
+        <input 
+          className={`input input-bordered border-1 w-full ${errors.password && touched.password && 'input-error'}`} 
+          type="password" 
+          name="password" 
+          placeholder="Password" 
+          onChange={handleChange} 
+          onBlur={handleBlur}
+          value={values.password} 
+        />
+        {errors.password && touched.password && (
+            <label className="label">
+              <span className="label-text-alt text-error"></span>
+              <span className="label-text-alt text-error">{errors.password}</span>
+            </label>
+        )}
+
+      </div>
+      <div className="text-right">
+        <Link className="hover:text-secondary bold text-[#38291B]"to="/forgot-password">Forgot Password?</Link>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <div className="w-full text-center">
+          <button disabled={isSubmitting} type="submit" className="btn btn-accent hover:btn-secondary btn-block normal-case">Sign In</button>
+        </div>
+        <div>or sign in with</div>
+      </div>
+      <div className="flex justify-center gap-5">
+        <button className="btn btn-primary bg-white border-primary hover:btn-secondary w-24">
+          <FcGoogle size={25} />
+        </button>
+        <button className="btn btn-primary bg-white border-primary hover:btn-secondary w-24">
+          <FaFacebook color='#4267B2' size={25} />
+        </button>
+      </div>
+    </form>
+  )
+}
+
+FormLogin.propTypes = {
+  values: propTypes.objectOf(propTypes.string),
+  errors: propTypes.objectOf(propTypes.string),
+  touched: propTypes.objectOf(propTypes.bool),
+  handleBlur: propTypes.func,
+  handleChange: propTypes.func,
+  handleSubmit: propTypes.func,
+  isSubmitting: propTypes.bool
+
+}
+
+const Login = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const token = useSelector(state => state.auth.token)
+  
+  
+
+  React.useEffect(()=> {
+    if(token){
+      console.log("test")
+      navigate("/")
     }
   }, [token, navigate])
 
+  const doLogin = async (values, {setSubmitting, setErrors}) => {
+    dispatch(clearMessage())
+    try{
+      const {email, password} = values
+      const body = new URLSearchParams({email, password}).toString() //mengambil query string
+      const {data} = await http().post('/auth/login', body)
+      dispatch(login(data.results.token))
+      setSubmitting(false)
+    }catch (err) {
+      const message = err?.response?.data?.message
+      if(message){
+        if(err?.response?.data?.results){
+          setErrors({
+            email: err.response.data.results.filter(item => param === "email")[0].message,
+            password: err.response.data.results.filter(item => param === "password")[0].message
+          })
+        }else{
+          dispatch(setErrorMessage(message))
+        }
+      } 
+    }
+  }
+  
   return(
 
     <div className="flex h-screen">
@@ -60,52 +161,21 @@ const Login = () => {
             <span className="text-3xl font-bold text-secondary">tick</span>
           </div>
         </Link>
-        <div className="w-[80%] flex flex-col gap-5">
-          <div className="text-3xl font-bold">Sign In</div>
-          <div className="">Hi, Welcome back to Urticket! </div>
-          {errorMessage && <div>
-            <div className="alert alert-error">{errorMessage}</div>
-          </div>}
-          {warningMessage && <div>
-            <div className="alert alert-warning">{location.state?.warningMessage}</div>
-          </div>}
-        </div>
-        <form onSubmit={doLogin} className="w-[80%] flex flex-col gap-5">
-          <div>
-            <input onFocus={()=>setWarningMessage('')} className="input input-bordered border-1 w-full" type="text" name="username" placeholder="Username" />
-          </div>
-          <div>
-            <input onFocus={()=>setWarningMessage('')} className="input input-bordered border-1 w-full" type="email" name="email" placeholder="Email" />
-          </div>
-          <div>
-            <input onFocus={()=>setWarningMessage('')} className="input input-bordered border-1 w-full" type="password" name="password" placeholder="Password" />
-          </div>
-          <div className="text-right">
-            <Link className="hover:text-secondary bold text-[#38291B]"to="/forgot-password">Forgot Password</Link>
-          </div>
-          <div className="flex flex-col justify-center items-center gap-5">
-            <div className="w-full text-center">
-              <button className="btn btn-accent hover:btn-secondary btn-block normal-case">Sign In</button>
-            </div>
-            <div>or sign in with</div>
-          </div>
-          <div className="flex justify-center gap-5">
-            <button className="btn btn-primary bg-white border-primary hover:btn-secondary w-24">
-              <FcGoogle size={25} />
-            </button>
-            <button className="btn btn-primary bg-white border-primary hover:btn-secondary w-24">
-              <FaFacebook color='#4267B2' size={25} />
-            </button>
-          </div>
-        </form>
+        <Formik
+          initialValues={{
+            email: '',
+            password: ''
+          }}
+          validationSchema={validationSchema}
+          onSubmit={doLogin}
+        >
+          {(props)=>(
+            <FormLogin {...props}/>
+          )}
+        </Formik>
       </div>
-      
-      
     </div>
-  
   )
-  
-
   
 }
 export default Login;
