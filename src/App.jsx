fafc517694424ba3da5d49eae33d05b4f43f6553
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-05-13 21:06:53 +0700

    add: provider redux, store redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
 
 import Home from "./pages/Home"
 import Details from "./pages/DetailEvent"
@@ -9,22 +10,26 @@ import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
 
+import {store} from './redux/store'
+
 const App = ()=> {
   
   return(
-    <BrowserRouter>
-    <Routes>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
 
-      <Route path="/" element={<Home />} />
-      <Route path="/event-detail/:id" element={<Details />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-        
-      
-    </Routes>
-    </BrowserRouter>
+          <Route path="/" element={<Home />} />
+          <Route path="/event-detail/:id" element={<Details />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+            
+          
+        </Routes>
+      </BrowserRouter>
+    </Provider>
     
   )
 }
