774af3b176b558cd80eb0b221496a413f647fdc9
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-08-14 06:56:37 +0700

    change cyclic env backend

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,24 +1,27 @@
-import axios from "axios"
+import axios from "axios";
 
 const http = (token, fallback) => {
-  const headers = {}
-  if(token){
-    headers.Authorization = `Bearer ${token}`
+  const headers = {};
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
   }
   const instance = axios.create({
     headers,
-    baseURL: import.meta.env.BACKEND_URL || "http://localhost:8888"
-  })
+    baseURL:
+      import.meta.env.BACKEND_URL || "https://nervous-waders-cod.cyclic.app",
+  });
 
-  instance.interceptors.response.use((response)=>{
-    return response
-  }, (err)=> {
-    if(err.response.status === 401){
-      return Promise.reject(fallback(err.response.data.message))
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (err) => {
+      if (err.response.status === 401) {
+        return Promise.reject(fallback(err.response.data.message));
+      }
     }
-  })
+  );
 
-  return instance
-  
-}
-export default http
+  return instance;
+};
+export default http;
