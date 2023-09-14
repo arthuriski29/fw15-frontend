4c40ca3badf1fc1f72e7165d830b5f213930dac6
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-09-14 19:47:28 +0700

    fix: wishlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,83 @@
 // import React from 'react'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Provider } from "react-redux"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import Home from "./pages/Home"
-import SearchResults from "./pages/SearchResults"
-import Details from "./pages/DetailEvent"
-import Profile from "./pages/Profile"
-import Login from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import ForgotPassword from "./pages/ForgotPassword"
-import ManageEvent from "./pages/ManageEvent"
-import SelectSection from "./pages/SelectSection"
-import Payment from "./pages/Payment"
-import MyBooking from "./pages/MyBooking"
+import Home from "./pages/Home";
+import SearchResults from "./pages/SearchResults";
+import Details from "./pages/DetailEvent";
+import Profile from "./pages/Profile";
+import Login from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import ManageEvent from "./pages/ManageEvent";
+import SelectSection from "./pages/SelectSection";
+import Payment from "./pages/Payment";
+import MyBooking from "./pages/MyBooking";
+import MyWishlist from "./pages/MyWishlist";
 
-import {store, persistor} from "./redux/store"
-import PrivateRoute from "./components/PrivateRoute"
-import { PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from "./redux/store";
+import PrivateRoute from "./components/PrivateRoute";
+import { PersistGate } from "redux-persist/integration/react";
 
-const App = ()=> {
-  
-  return(
+const App = () => {
+  return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
           <Routes>
-
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<SearchResults />} />
             <Route path="/event-detail/:id" element={<Details />} />
-            <Route path="/profile" 
+            <Route
+              path="/profile"
               element={
                 <PrivateRoute>
                   <Profile />
                 </PrivateRoute>
-              } 
+              }
             />
             <Route path="/sign-in" element={<Login />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/manage-event" element={<ManageEvent />} />
-            <Route path="/select-section/:id" 
+            <Route
+              path="/select-section/:id"
               element={
                 <PrivateRoute>
                   <SelectSection />
                 </PrivateRoute>
-              } 
+              }
             />
-            <Route path="/payment" 
+            <Route
+              path="/payment"
               element={
                 <PrivateRoute>
                   <Payment />
                 </PrivateRoute>
-              } 
+              }
             />
-            <Route path="/mybooking" 
+            <Route
+              path="/mybooking"
               element={
                 <PrivateRoute>
                   <MyBooking />
                 </PrivateRoute>
-              } 
+              }
+            />
+            <Route
+              path="/mywishlist"
+              element={
+                <PrivateRoute>
+                  <MyWishlist />
+                </PrivateRoute>
+              }
             />
           </Routes>
         </BrowserRouter>
       </PersistGate>
     </Provider>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
